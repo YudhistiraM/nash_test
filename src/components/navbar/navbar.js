4bc5484f2635077ea45a9519134428d9d6f06d63
YudhitiraM <ma.yudhistira@hotmail.com>
2020-09-08 04:11:15 +0700

    Update Nav Bar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -6,13 +6,13 @@ function NavBar() {
     return (
         <div>
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-                <Navbar.Brand href="#home">Navbar Brand</Navbar.Brand>
+                <Navbar.Brand href="../../home">Navbar Brand</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="mr-auto"></Nav>
                     <Nav>
-                        <Nav.Link href="#deets">+ Add Event</Nav.Link>
-                        <Nav.Link eventKey={2} href="#memes">Dashboard</Nav.Link>
+                        <Nav.Link href="../../add">+ Add Event</Nav.Link>
+                        <Nav.Link href="../../dashboard">Dashboard</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
