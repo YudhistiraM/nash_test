5f05f455a277a74150ed37ae79ca2c939bc3e416
YudhitiraM <ma.yudhistira@hotmail.com>
2020-09-07 21:22:24 +0700

    Initial home

---
+++ src/components/home/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div className="App">
+            <p>
+                Hello world!
+            </p>
+        </div>
+    );
+}
+
+export default Home;
