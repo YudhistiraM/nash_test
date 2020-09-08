69d218f20476076389a55d160ad7107922cb9d35
YudhitiraM <ma.yudhistira@hotmail.com>
2020-09-08 13:39:05 +0700

    Creat add data API

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,17 +1,53 @@
 var express = require('express');
 var router = express.Router();
 
-function getJson(){
+const readFile = () => {
   const fs = require('fs');
-  let rawdata = fs.readFileSync('./data.json');
-  let data = JSON.parse(rawdata);
-  return data;
+  var rawdata = fs.readFileSync('data.json', 'utf8');
+  return JSON.parse(rawdata);
 }
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  let data = getJson();
-  res.json(data);
+const writeFile = (content) => {
+  const fs = require('fs');
+  fs.writeFileSync("./data.json", JSON.stringify(content, null, 3), 'utf8');
+}
+
+router.get('/', function (req, res, next) {
+  try {
+    let data = readFile();
+    res.json(data);
+  } catch (err) {
+    res.json({
+      error: true,
+      message: `something went wrong : ${err.message}`
+    })
+  }
+});
+
+router.post('/', function (req, res, next) {
+  try {
+    var input = readFile();
+    var value = {
+      id: req.body.id,
+      title: req.body.title,
+      location: req.body.location,
+      date: req.body.date,
+      participants: req.body.participants.toString().split(','),
+      note: req.body.note,
+      img: null
+    }
+    input.push(value);
+    writeFile(input);
+    res.json({
+      success: true,
+      message: `Data has been added succesfully`
+    })
+  } catch (err) {
+    res.json({
+      error: true,
+      message: `something went wrong : ${err.message}`
+    })
+  }
 });
 
 module.exports = router;
