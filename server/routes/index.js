1c50188ac1e2e27cc5f069203f40b8646a47a466
YudhitiraM <ma.yudhistira@hotmail.com>
2020-09-08 12:58:59 +0700

    Create API load data from json

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+function getJson(){
+  const fs = require('fs');
+  let rawdata = fs.readFileSync('./data.json');
+  let data = JSON.parse(rawdata);
+  return data;
+}
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let data = getJson();
+  res.json(data);
 });
 
 module.exports = router;
